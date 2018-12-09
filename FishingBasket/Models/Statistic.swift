b969ae85868a8059e06554a3eea7b9e79f52d88b
MAXOPKA <d.n.krivenko@gmail.com>
2018-12-09 23:34:22 +0500

    Added Statistic model

---
+++ FishingBasket/Models/Statistic.swift
@@ -0,0 +1,15 @@
+//
+//  Statistic.swift
+//  FishingBasket
+//
+//  Created by Дмитрий Кривенко on 09/12/2018.
+//  Copyright © 2018 Дмитрий Кривенко. All rights reserved.
+//
+
+import Foundation
+
+class Statistic {
+  public func agregate(by: String) {
+    
+  }
+}
